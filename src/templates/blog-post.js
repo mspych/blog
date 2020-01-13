9e5c0da493acc508d8e84d4a352c55c3049e11a7
Laurie Barth <laurie@LauriesrkLaptop.fios-router.home>
2020-01-13 15:31:22 -0500

    Add estimated reading time to the heading of each post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,7 +10,7 @@ class BlogPostTemplate extends React.Component {
   render() {
     const post = this.props.data.markdownRemark
     const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
+    const { previous, next, readingTime } = this.props.pageContext
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -35,7 +35,7 @@ class BlogPostTemplate extends React.Component {
                 marginBottom: rhythm(1),
               }}
             >
-              {post.frontmatter.date}
+              {post.frontmatter.date} - {readingTime}
             </p>
           </header>
           <section dangerouslySetInnerHTML={{ __html: post.html }} />
