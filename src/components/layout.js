b0558685210ad827f9a519800d6ebb12cbe1e889
mspych <57863176+mspych@users.noreply.github.com>
2020-05-11 11:06:02 +0100

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -62,11 +62,13 @@ class Layout extends React.Component {
       >
         <header>{header}</header>
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+<footer class="layout__Footer-mdb7ar-1 iHTkom">2020, © <a href="https://www.chenmor.com">Chen Mor</a>&nbsp;&nbsp;|&nbsp;Build something great 🛠</footer>
+2020
+,
+© 
+<a href="https://www.chenmor.com">Chen Mor</a>
+&nbsp;&nbsp;|&nbsp;Build something great 🛠
+<footer class="layout__Footer-mdb7ar-1 iHTkom">2020, © <a href="https://www.chenmor.com">Chen Mor</a>&nbsp;&nbsp;|&nbsp;Build something great 🛠</footer>
       </div>
     )
   }
